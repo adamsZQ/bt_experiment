86228f7c1001d932ade222c9366c97cbf7d34e8d
adamszhu <adamszhu97@gmail.com>
2019-03-11 13:01:59 +0800

    a

--- data/glove_h5.py
+++ data/glove_h5.py
@@ -1,4 +1,6 @@
 """Convert raw GloVe word vector text file to h5."""
+import argparse
+
 import h5py
 import numpy as np
 
@@ -69,3 +71,14 @@ def glove_h5(file_prefix):
 
     f = h5py.File(file_prefix + 'embeddings/glove.840B.300d.h5', 'a')
     f.create_dataset(data=vocab, name='words_flatten')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--prefix") # data and model prefix
+    args = parser.parse_args()
+
+    HIDDEN_DIM = 4
+    FILE_PREFIX = args.prefix
+
+    glove_h5(FILE_PREFIX)
